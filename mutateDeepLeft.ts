8b91e1edeac24069ea2ebc73961b766bc7d3bcb4
Stefan Terdell <stefan.terdell@live.com>
2020-07-05 12:44:51 +0200

    :bug:

--- mutateDeepLeft.ts
+++ mutateDeepLeft.ts
@@ -1,4 +1,4 @@
 export const mutateDeepLeft = (left, right) =>
   Object.keys(right).forEach((key) =>
-    typeof right[key] === 'object' ? mutateDeepLeft((left[key] !== undefined || (left[key] = {})) && left[key], right[key]) : (left[key] = right[key]),
+    typeof right[key] === 'object' ? mutateDeepLeft((typeof left[key] === 'object' || (left[key] = {})) && left[key], right[key]) : (left[key] = right[key]),
   )
