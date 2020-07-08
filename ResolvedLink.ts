156db505847eff818805385e84cb4b5d93cb9d53
Stefan Terdell <stefan.terdell@live.com>
2020-07-08 11:48:34 +0200

    :sparkles: Default data prop types for resolved entitites

--- ResolvedLink.ts
+++ ResolvedLink.ts
@@ -1,6 +1,6 @@
 import { Node } from './Node'
 import { ResolvedNode } from './ResolvedNode'
-export interface ResolvedLink<LinkData, NodeData> extends Node<LinkData> {
+export interface ResolvedLink<LinkData = any, NodeData = LinkData> extends Node<LinkData> {
   to: ResolvedNode<NodeData, LinkData>
   from: ResolvedNode<NodeData, LinkData>
 }
--- ResolvedNode.ts
+++ ResolvedNode.ts
@@ -1,6 +1,6 @@
 import { Node } from './Node'
 import { ResolvedLink } from './ResolvedLink'
-export interface ResolvedNode<NodeData, LinkData> extends Node<NodeData> {
+export interface ResolvedNode<NodeData = any, LinkData = NodeData> extends Node<NodeData> {
   to: ResolvedLink<LinkData, NodeData>[]
   from: ResolvedLink<LinkData, NodeData>[]
 }
